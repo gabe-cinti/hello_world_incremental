e5f987c4ee255f32ede3440c7b61829aae17a848
gabe-cinti <gabrielcinti@hotmail.com>
2023-11-07 16:41:56 -0300

    Otimização e limpeza de código. Adição de comentários.
    Signed-off-by: gabe-cinti <gabrielcinti@hotmail.com>
    
    Change-Id: Ieb84f26e92a82f72ff01e4843b702a33afc41b6e

--- src/controle/Principal.java
+++ src/controle/Principal.java
@@ -2,23 +2,19 @@ package controle;
 
 public class Principal {
     public static void main(String[] args) {
-	String objetivo = "hello world";
+	String objetivo = "Hello World";
     String atual = "";
 
-    for (int i = 0; i < objetivo.length(); i++) {
-        char objetivoChar = objetivo.charAt(i);
-
-        while (atual.length() <= i) {
-            atual += 'a'; // Comece com 'a' e aumente gradualmente
-        }
-
-        while (atual.charAt(i) != objetivoChar) {
-            System.out.println(atual);
-            char[] arranjo = atual.toCharArray();
-            arranjo[i]++;
-            atual = new String(arranjo);
-        }
+    	for (int i = 0; i < objetivo.length(); i++) {
+    		atual += 'a'; // Comece com 'a' e aumente gradualmente
+        
+    		while (atual.charAt(i) != objetivo.charAt(i)) {
+    			System.out.println(atual); // Imprime a String atual
+    			char[] arranjo = atual.toCharArray(); // Incorpora a última letra da String atual para o arranjo.
+    			arranjo[i]++; // Incrementa a letra do arranjo
+    			atual = new String(arranjo);  // Incorpora a String atual com os caracteres contidos em arranjo
+    		}
+    	}
+    System.out.println(atual); // Imprime uma ultima vez a String atual
     }
-    System.out.println(atual);
-}
 }
